b3ccd10deebdf5c4ddd12253d1baddd16bf7b9a2
BrandieSmith <smith6bs@mail.uc.edu>
2025-10-18 22:45:20 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,86 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final Random RND = new Random();
+    private static final Scanner INPUT = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        System.out.println("Let's Play Craps");
+
+        boolean playAgain = true;
+        while (playAgain) {
+            System.out.println("\n-- New Game: First Roll --");
+            int[] roll = rollDice();
+            int sum = roll[0] + roll[1];
+            printRoll(roll[0], roll[1], sum);
+
+            if (sum == 2 || sum == 3 || sum == 12)
+            {
+                System.out.println("Craps! You crapped out and lost.");
+            } else if (sum == 7 || sum == 11)
+            {
+                System.out.println("Natural! You win!");
+            } else {
+                int point = sum;
+                System.out.println("Point is set to " + point + ".");
+                System.out.println("Trying for point...");
+
+                boolean rolling = true;
+                while (rolling)
+                {
+                    roll = rollDice();
+                    sum = roll[0] + roll[1];
+                    printRoll(roll[0], roll[1], sum);
+
+                    if (sum == point)
+                    {
+                        System.out.println("Made point and won!");
+                        rolling = false;
+                    } else if (sum == 7)
+                    {
+                        System.out.println("Got a seven and lost.");
+                        rolling = false;
+                    } else {
+                        System.out.println("Trying for point...");
+                    }
+                }
+            }
+
+            playAgain = promptYesNo("Do you want to play again? [Y/N]: ");
+        }
+
+        System.out.println("Thanks for playing! Goodbye.");
+
+    }
+
+    private static int[] rollDice()
+    {
+        int die1 = RND.nextInt(6) + 1; // 1..6
+        int die2 = RND.nextInt(6) + 1; // 1..6
+        return new int[]{die1, die2};
+    }
+
+    private static void printRoll(int die1, int die2, int sum)
+    {
+        System.out.println("Rolled -> Die 1: " + die1 + ", Die 2: " + die2 + " | Sum: " + sum);
+    }
+
+    private static boolean promptYesNo(String message)
+    {
+        while (true)
+        {
+            System.out.print(message);
+            if (!INPUT.hasNextLine()) {
+                System.out.println(); // newline
+                return false;
+            }
+            String ans = INPUT.nextLine().trim();
+            if (ans.equalsIgnoreCase("Y")) return true;
+            if (ans.equalsIgnoreCase("N")) return false;
+            // blank or anything else -> reprompt
+            System.out.println("Please enter Y or N.");
         }
     }
 }
